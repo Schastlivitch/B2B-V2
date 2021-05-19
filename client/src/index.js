bbd4994469f3102aaa03b020098dda05538b591b
Ivan <schastlivitch@gmail.com>
2021-05-19 20:48:56 +0300

    Chat - beginning

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import 'firebase/firestore'
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCAcyGP3RZP2Z-mEvqgDCBbWzW4w5h61A",
+  authDomain: "chat-react-ec2e6.firebaseapp.com",
+  projectId: "chat-react-ec2e6",
+  storageBucket: "chat-react-ec2e6.appspot.com",
+  messagingSenderId: "834110186741",
+  appId: "1:834110186741:web:0b416afb246b96adc2d1bc",
+  measurementId: "G-FMNC2GGZXL"
+};
+firebase.initializeApp(firebaseConfig);
+const firestore = firebase.firestore()
+const Context = createContext(null)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Context.Provider value={{ firebase, firestore }}>
+        <App />
+      </Context.Provider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+export{
+  Context
+}
---
+++ client/src/modals/Chat/Chat.jsx
@@ -0,0 +1,76 @@
+import { useContext, useState } from "react"
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+import firebase from 'firebase';
+import { useSelector } from "react-redux";
+import { Context } from "../../index"
+
+const Chat = ({ partner }) => {
+
+  const { firestore } = useContext(Context)
+
+  const currentUser = useSelector(state => state.user)
+
+  const [messages, loading] = useCollectionData(firestore.collection('beermessages').orderBy('createdAt'))
+  const [input, setInput] = useState('')
+
+  const sendMessage = async (e) => {
+    firestore.collection('beermessages').add({
+      senderID: currentUser._id,
+      recieverID: partner._id,
+      text: input,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    setInput('')
+  }
+
+
+  return (
+    <>
+      <div className="modal fade" id={`getChat${partner._id}`} tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4>Чат c {partner.title}</h4>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <div className="container">
+                <ul className="list-unstyled">
+                {
+                  messages?.length
+                    ?
+                    messages.map(message => {
+                      return (
+                        <>
+                          <li className={message.senderID === currentUser?._id ? 'out' : 'in'}>
+                            <div className="chat-body">
+                              <div className={message.senderID === currentUser?._id ? "chat-message d-flex flex-row-reverse" : "chat-message d-flex flex-row"}>
+                                <p>{message.text}</p>
+                              </div>
+                            </div>
+                          </li>
+                        </>
+                      )
+                    })
+                    :
+                    'Нет сообщений...'
+                }
+                </ul>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <div className="d-flex align-items-center justify-content-center">
+                <div className="container">
+                  <input className="form-control" type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+                </div>
+                <button className="btn" onClick={sendMessage}>Отправить</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Chat;
